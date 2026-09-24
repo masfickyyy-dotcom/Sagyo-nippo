7a6c7d3705a4b0c0bf9488f0b4d3e39dbe77cbee
masfickyyy-dotcom <masfickyyy@gmail.com>
2026-09-24 21:15:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL = 'https://abcd1234xyz.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+window.SUPABASE_ANON_KEY = 'window.SUPABASE_URL = 'https://jkvmsugtplqwbybyigta.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_...';
